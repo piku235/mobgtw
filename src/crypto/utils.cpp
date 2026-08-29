2bfe0e885c76b8e2f08f2585a03f96118c0a095f
Piotr Kugla <piku235@gmail.com>
2026-08-29 19:05:35 +0200

    Use std::bit_cast in timestamp2iv

--- src/crypto/utils.cpp
+++ src/crypto/utils.cpp
@@ -3,15 +3,24 @@
 #include <arpa/inet.h>
 #include <openssl/evp.h>
 
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <cstddef>
+
 namespace jungi::mobgtw::crypto {
 
 bytes timestamp2iv(const time_t timestamp)
 {
     bytes iv(EVP_MAX_IV_LENGTH, '\0');
-    uint32_t bigEndianTimestamp = htonl(static_cast<uint32_t>(timestamp));
-    byte* start = reinterpret_cast<byte*>(&bigEndianTimestamp);
+    auto bigEndianTimestamp = htonl(static_cast<uint32_t>(timestamp));
+    auto timestampBytes = std::bit_cast<std::array<byte, sizeof(bigEndianTimestamp)>>(bigEndianTimestamp);
 
-    iv.insert(iv.begin() + (EVP_MAX_IV_LENGTH - sizeof(uint32_t)), start, start + sizeof(uint32_t));
+    std::copy(
+        timestampBytes.begin(),
+        timestampBytes.end(),
+        iv.end() - timestampBytes.size()
+    );
 
     return iv;
 }
